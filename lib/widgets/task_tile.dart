aed3cb9ccca207eaf40c1e08ea9e9add7880b8b4
freitaspedro <pfreitas@id.uff.br>
2023-05-24 14:27:48 -0300

    add checkbox change state treatment

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool? isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {  },
+      title: Text(
+        'This is a task',
+        style: TextStyle(decoration: isChecked! ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked!,
+          toggleCheckboxState: (bool? checkboxState) {
+            setState(() {
+              isChecked = checkboxState;
+            });
+          },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+
+  final bool checkboxState;
+  final Function(bool?)? toggleCheckboxState;
+
+  const TaskCheckbox({required this.checkboxState, this.toggleCheckboxState, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.purple,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
